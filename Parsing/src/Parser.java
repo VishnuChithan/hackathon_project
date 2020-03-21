d673d8e517d431abc5cfecb997e0e559144dd044
Vishnu <Vishnu@LAPTOP-1GCCOBIO>
2020-03-21 09:17:17 +0530

    Third Commit

--- Parsing/src/Parser.java
+++ Parsing/src/Parser.java
@@ -1,34 +1,31 @@
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.DecimalFormat;
-
-import org.json.*;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import com.mysql.cj.xdevapi.JsonArray;
-
+import java.math.BigDecimal;
+import java.sql.*;
+import org.json.simple.*;
 import java.util.StringTokenizer;
-
 public class Parser {
-
+	BigDecimal formatchanger(String param)
+	{
+		BigDecimal value=new BigDecimal(param);
+		value=value.setScale(2,BigDecimal.ROUND_HALF_UP);
+		return value;
+	}
 	public static void main(String[] args) throws IOException, SQLException {
-		DecimalFormat df = new DecimalFormat("#.##");
+		Parser parse=new Parser();
 		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/Parsing","root","Password-1");
-		System.out.println("Database connection successful!\n");
+		System.out.println("Database connection successful!");
 		Statement stm=conn.createStatement();
-		stm.executeUpdate("create table data(transaction varchar(64),time DECIMAL(10,2));");
-		System.out.println("\n Table created");
+		stm.executeUpdate("create table if not exists data(transaction varchar(64),time DECIMAL(10,2));");
+		System.out.println("\n Table created\n");
 		File file = new File("input.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String eachline;
 		int transaction=0;
-		JsonArray obj1=new JsonArray();
+		JSONArray arr=new JSONArray();
 		JSONObject obj=new JSONObject();
+		JSONObject obj1=new JSONObject();
+		JSONObject obj2=new JSONObject();
+		System.out.println("Inserting Values into the Database.. Please Wait...");
 		while((eachline=br.readLine())!=null)
 		{	
 			transaction++;
@@ -40,18 +37,36 @@ public class Parser {
 				st.nextToken();
 			}
 			stm.executeUpdate("insert into data(transaction,time)values('"+transaction+"s',"+st.nextToken()+");");	
-			System.out.println("Inserting..");
 		}
 		ResultSet rs=stm.executeQuery("select * from data");
 		while(rs.next())
 		{
 			String key=rs.getString("transaction");
-			float val=Float.parseFloat(df.format(rs.getFloat("time")));
-			obj.put(key,val);
+			obj.put(key,parse.formatchanger(rs.getString("time")));
 		}
+		obj1.put("value",obj);
+		obj2.put("sampletransaction",obj1);
+		rs = stm.executeQuery("select max(time) from data"); 
+		if(rs.next())
+		{
+			obj1.put("maxcpu",parse.formatchanger(rs.getString(1)));
+		}
+		rs = stm.executeQuery("select avg(time) from data"); 
+		if(rs.next())
+		{
+			obj1.put("avgcpu",parse.formatchanger(rs.getString(1)));
+		}
+		arr.add(0,obj2);
 		FileWriter file1=new FileWriter("result.json");
-		file1.write(obj.toString());
+		file1.write(arr.toString());
 		file1.close();
-		System.out.println("Data Added Successfully!");
+		System.out.println("\nData Added Successfully!\n");
+		File file3 = new File("result.json");
+		BufferedReader br1 = new BufferedReader(new FileReader(file3));
+		String line;
+		while((line=br1.readLine())!=null)
+		{
+			System.out.println(line);
+		}
 	}
 }
